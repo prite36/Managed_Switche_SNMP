3362f25d3712000d3d151a798cdf7f29bce38059
SPRITELEMON\Spritelemon <spritelemon36@hotmail.com>
2018-03-02 03:31:38 +0700

    no message

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/* eslint-disable*/
+/* eslint-disable */
 var express = require('express')
 var app = express()
 var snmp = require('snmp-native')
@@ -10,121 +10,112 @@ app.use(bodyParser.urlencoded({ extended: false }))
 var community = 'private'
 // ///// set interval ////////
 ///// variable ////////
-var infoR415 = []
-var int_415 = []
-var port_415 = []
-var time_415 = []
-var inOctet_415 = []
-var outOctet_415 = []
+var infoSW = []
+var int_sw = []
+var port_sw = []
+var time_sw = []
+var inOctet_sw = []
+var outOctet_sw = []
 var data415 = []
 
-var infoR402 = []
-var int_402 = []
-var port_402 = []
-var time_402 = []
-var inOctet_402 = []
-var outOctet_402 = []
-var data402 = []
-
 ///////////////////// INFO 415 //////////////////////////////
-app.get('/info415', function (req, res) {
-  var getInfoR415 = new snmp.Session({ host: '10.4.15.210', community: community })
+app.get('/infoSW', function (req, res) {
+  var getInfoSW = new snmp.Session({ host: '10.4.15.210', community: community })
   var oidget_info = '.1.3.6.1.2.1.1'
-  getInfoR415.getSubtree({ oid: oidget_info }, function (err, varbinds) {
-    infoR415.push({
+  getInfoSW.getSubtree({ oid: oidget_info }, function (err, varbinds) {
+    infoSW.push({
           discription: varbinds[0].value,
           uptime: timecheck(varbinds[2].value),
           name: varbinds[4].value
     })
 
-    getInfoR415.close()
+    getInfoSW.close()
     //console.log(varbinds[0].value);
   })
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headres', 'X-Requested-With')
-  res.send(infoR415)
-  infoR415 = []
+  res.send(infoSW)
+  infoSW = []
 })
 
 
-//////////////////////// R415 //////////////////////////
-app.get('/415', function (req, res) {
-  ////////////r415////////////////////
-
+//////////////////////// SW //////////////////////////
+app.get('/ifStatus', function (req, res) {
+  ////////////SW////////////////////
 
  //////// index //////
-  var getintR415 = new snmp.Session({ host: '10.4.15.210', community: community })
+  var getintSW = new snmp.Session({ host: '10.4.15.210', community: community })
   var oidget_int = '.1.3.6.1.2.1.2.2.1.2'
-  getintR415.getSubtree({ oid: oidget_int }, function (err, varbinds) {
+  getintSW.getSubtree({ oid: oidget_int }, function (err, varbinds) {
     varbinds.forEach(function (data) {
-      int_415.push(data.value.replace('GigabitEthernet','Gig '))
+      int_sw.push(data.value.replace('GigabitEthernet','Gig '))
     })
-    getintR415.close()
+    getintSW.close()
   })
   // /////// portstatus  ////////////
-  var getportR415 = new snmp.Session({ host: '10.4.15.210', community: community })
+  var getportSW = new snmp.Session({ host: '10.4.15.210', community: community })
   var oidget_port = '.1.3.6.1.2.1.2.2.1.8'
-  getportR415.getSubtree({ oid: oidget_port }, function (err, varbinds) {
+  getportSW.getSubtree({ oid: oidget_port }, function (err, varbinds) {
     varbinds.forEach(function (data) {
       // console.log(data.value)
         if (data.value == 1) {
-           port_415.push("up")
+           port_sw.push("up")
         }
         else if (data.value == 2) {
-          port_415.push("down")
+          port_sw.push("down")
         }
     })
-    getportR415.close()
+    getportSW.close()
   })
 
   // .1.3.6.1.2.1.2.2.1.9
-  var gettimeR415 = new snmp.Session({ host: '10.4.15.210', community: community })
+  var gettimeSW = new snmp.Session({ host: '10.4.15.210', community: community })
   var oidget_time = '.1.3.6.1.2.1.2.2.1.9'
-  gettimeR415.getSubtree({ oid: oidget_time }, function (err, varbinds) {
+  gettimeSW.getSubtree({ oid: oidget_time }, function (err, varbinds) {
     varbinds.forEach(function (data) {
-      time_415.push(timecheck(data.value))
+      time_sw.push(timecheck(data.value))
     })
-    gettimeR415.close()
+    gettimeSW.close()
   })
 
   // .1.3.6.1.2.1.2.2.1.10
-  var getInR415 = new snmp.Session({ host: '10.4.15.210', community: community })
+  var getInSW = new snmp.Session({ host: '10.4.15.210', community: community })
   var oidget_in = '.1.3.6.1.2.1.2.2.1.10'
-  getInR415.getSubtree({ oid: oidget_in }, function (err, varbinds) {
+  getInSW.getSubtree({ oid: oidget_in }, function (err, varbinds) {
     varbinds.forEach(function (data) {
 
-      inOctet_415.push(convert(data.value))
+      inOctet_sw.push(convert(data.value))
     })
-    getInR415.close()
+    getInSW.close()
   })
 
   // .1.3.6.1.2.1.2.2.1.16
-  var getOutR415 = new snmp.Session({ host: '10.4.15.210', community: community })
+  var getOutSW = new snmp.Session({ host: '10.4.15.210', community: community })
   var oidget_out = '.1.3.6.1.2.1.2.2.1.16'
-  getOutR415.getSubtree({ oid: oidget_out }, function (err, varbinds) {
+  getOutSW.getSubtree({ oid: oidget_out }, function (err, varbinds) {
     varbinds.forEach(function (data) {
 
-      outOctet_415.push(convert(data.value))
+      outOctet_sw.push(convert(data.value))
     })
-    getOutR415.close()
+    getOutSW.close()
   })
-  int_415.forEach(function (err, index) {
+  int_sw.forEach(function (err, index) {
     var set = {
       name: '415',
-      int: int_415[index],
-      port: port_415[index],
-      time: time_415[index],
-      inOctet: inOctet_415[index],
-      outOctet: outOctet_415[index]
+      int: int_sw[index],
+      port: port_sw[index],
+      time: time_sw[index],
+      inOctet: inOctet_sw[index],
+      outOctet: outOctet_sw[index]
     }
 
     data415.push(set)
   })
-  int_415= []
-  port_415 =[]
-  time_415 =[]
-  inOctet_415 = []
-  outOctet_415= []
+  int_sw= []
+  port_sw =[]
+  time_sw =[]
+  inOctet_sw = []
+  outOctet_sw= []
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headres', 'X-Requested-With')
   res.send(data415)
